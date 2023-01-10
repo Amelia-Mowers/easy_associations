2078d1d6286803a4fe5c8d601a6f098fb0c1249b
Amelia-Mowers <105567018+Amelia-Mowers@users.noreply.github.com>
2023-01-09 22:37:59 -0800

    Cleaned up and made it so you can't have multiple copies of child in parent

--- src/bidirectional_one_to_many.rs
+++ src/bidirectional_one_to_many.rs
@@ -11,22 +11,21 @@ macro_rules! bidirectional_one_to_many {
     ) => {
         impl $one_struct {
             pub fn $add_many_to_one_func(&mut self, many_ref: Rc<RefCell<$many_struct>>) {
+                self.$one_struct_many_field.retain(|x| !Rc::ptr_eq(x, &many_ref));
                 self.$one_struct_many_field.push(many_ref.clone());
-                let mut many = many_ref.borrow_mut();
-                many.$many_struct_one_field = Some(self.clone_self_ref());
+                many_ref.borrow_mut().$many_struct_one_field = Some(self.clone_self_ref());
             }
 
             pub fn $remove_many_from_one_func(&mut self, many_ref: Rc<RefCell<$many_struct>>) {
-                self.$one_struct_many_field.retain(|x| x != &many_ref);
-                let mut many = many_ref.borrow_mut();
-                many.$many_struct_one_field = None;
+                self.$one_struct_many_field.retain(|x| !Rc::ptr_eq(x, &many_ref));
+                many_ref.borrow_mut().$many_struct_one_field = None;
             }
         }
 
         impl $many_struct {
             pub fn $set_one_for_many_func(&mut self, one_ref: Option<Rc<RefCell<$one_struct>>>) {
-                let s = &self.clone_self_ref();
                 if let Some(o) = &self.$many_struct_one_field {
+                    let s = &self.clone_self_ref();
                     o.borrow_mut().$one_struct_many_field.retain(|x| !Rc::ptr_eq(x, s));
                 }
 
